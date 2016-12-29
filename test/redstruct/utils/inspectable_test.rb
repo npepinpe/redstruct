0601df994c220a0b11f9c965b7b92422c06a245d
Nicolas Pepin-Perreault <nicolas.pepin-perreault@offerista.com>
2016-12-29 16:13:45 +0100

    added utils/inspectable tests

--- test/redstruct/utils/inspectable_test.rb
+++ test/redstruct/utils/inspectable_test.rb
@@ -1,9 +1,30 @@
 # frozen_string_literal: true
 require 'test_helper'
+require 'flexmock/minitest'
 
 module Redstruct
   module Utils
     class InspectableTest < Redstruct::Test
+      def test_inspect
+        child = flexmock('test')
+        child.should_receive(:inspect).and_return('child').once
+        other = 'val'
+
+        object = self.class::Test.new(child: child, other: other)
+        assert_equal %(Redstruct::Utils::InspectableTest::Test: child: <child>, other: <"val">), object.inspect, 'should generate the correct inspect string, calling inspect on all values'
+      end
+
+      class Test
+        include Redstruct::Utils::Inspectable
+
+        def initialize(attrs)
+          @attrs = attrs
+        end
+
+        def inspectable_attributes
+          return @attrs
+        end
+      end
     end
   end
 end
