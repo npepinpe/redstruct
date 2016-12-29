5b17395f01b5f5b5eab93d0d791b13f2c2356487
Nicolas Pepin-Perreault <nicolas.pepin-perreault@offerista.com>
2016-12-29 12:35:56 +0100

    added factory/object tests

--- test/redstruct/factory/object_test.rb
+++ test/redstruct/factory/object_test.rb
@@ -4,6 +4,17 @@ require 'test_helper'
 module Redstruct
   class Factory
     class ObjectTest < Redstruct::Test
+      def test_initialize
+        factory = create_factory
+        object = Redstruct::Factory::Object.new(factory: factory)
+        assert_equal factory, object.factory, 'should have the given factory as the object factory'
+      end
+
+      def test_connection
+        factory = create_factory
+        object = Redstruct::Factory::Object.new(factory: factory)
+        assert_equal factory.connection, object.connection, 'should delegate #connection to the factory connection'
+      end
     end
   end
 end
